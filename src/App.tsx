5298e9648394a961585dfc5fb552a22bd8c188f7
AnaMigriauli <anamigriauli1994@gmail.com>
2024-02-29 01:01:10 +0400

    Install TanStack Query

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,22 @@
 import "./App.css";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import HomePage from "./HomePage";
 import HistoryPage from "./HistoryPage";
 import Navigation from "./Navigation";
 import { useState } from "react";
 
+const queryClient = new QueryClient();
+
 const App: React.FC = () => {
   const [currentPage, setCurrentPage] = useState<string>("home");
 
   return (
-    <div>
-      <Navigation setCurrentPage={setCurrentPage} />
-      {currentPage === "home" ? <HomePage /> : <HistoryPage />}
-    </div>
+    <QueryClientProvider client={queryClient}>
+      <div>
+        <Navigation setCurrentPage={setCurrentPage} />
+        {currentPage === "home" ? <HomePage /> : <HistoryPage />}
+      </div>
+    </QueryClientProvider>
   );
 };
 
